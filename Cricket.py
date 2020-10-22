101f559cd919e384cdaae77f037828eb77bf6bec
Schrodinger73 <63200376+Schrodinger73@users.noreply.github.com>
2020-10-22 10:29:49 +0530

    Update Cricket.py

--- Cricket.py
+++ Cricket.py
@@ -1,15 +1,18 @@
 import math
 import random
 
+# Sum of all values in an array
 def sum_array(a):
     sum = 0
     for i in range(0, len(a)):
         sum += a[i]
     return sum
 
+# Returns output of a random dice roll
 def dice():
     return random.randint(1, 6)
 
+# Returns score after 'n' balls, where '5' is "Out", and other values on the dice correspond to runs.
 def cricket(n):
     array = []
     i = 1
